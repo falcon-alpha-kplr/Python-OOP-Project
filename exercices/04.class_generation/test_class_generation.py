9eaaf9e5e4955b73da1b2cbb659b502a7ca0bd9f
falcon-alpha-kplr <124891098+falcon-alpha-kplr@users.noreply.github.com>
2023-03-09 22:27:51 +0000

    Anis

--- exercices/04.class_generation/test_class_generation.py
+++ exercices/04.class_generation/test_class_generation.py
@@ -89,7 +89,7 @@ def generate_class_def(nom_classe: str, attributs: dict, nom_superclasse: str, a
 
 """
 Code de Test / Exemple d'utilisation :
-Ce code génère la définition de la classe Voiture, qui hérite de la classe Vehicule et possède deux attributs, moteur et . 
+Ce code génère la définition de la classe Voiture, qui hérite de la classe Vehicule et possède deux attributs, moteur et nbportes. 
 Le constructeur de la classe prend en argument moteur, nbportes, marque et modele, ainsi que les arguments de la superclasse Vehicule. 
 La variable code_classe contient le code source généré par la fonction generate_class_def
 """
@@ -111,7 +111,7 @@ if __name__ == '__main__':
 
 # Autre exemple : 
 def test_function():
-    
+
     attributs = {
     "matériaux":"str",
     "couleur":"str",
